9531a63bf9285ed3997f855c660a0aafad35e0b6
DCPagan <aphrocraft@gmail.com>
2013-11-10 23:00:49 -0500

    added loop in main thread to prompt user for next proxy connection

--- main.c
+++ main.c
@@ -6,6 +6,8 @@ int main(int argc, char **argv){
 	unsigned short port;
 	struct sockaddr_in clientaddr;
 	struct hostent hostinfo;
+	char buf[64];
+	char *bufp=buf;
 	if(argc!=3&&argc!=4){
 		fprintf(stderr, "Error\n\t Usage for first proxy:\n\t\t"
 			"cs352proxy <port> <local interface> \n\t"
@@ -14,10 +16,11 @@ int main(int argc, char **argv){
 		return -1;
 	}
 	//	Initialize all socket descriptors as -1.
+	memset(buf, 0, sizeof(buf));
+	memset(&rio_eth, 0, sizeof(rio_eth));
 	for(i=0; i<CONNECTION_MAX; i++){
 		connections[i]=-1;
 		eth_tid[i]=0;
-		memset(&rio_eth, 0, sizeof(rio_eth));
 	}
 	i=0;
 	switch(argc){
@@ -82,6 +85,44 @@ int main(int argc, char **argv){
 	pthread_create(&tap_tid, NULL, tap_handler, &tapfd);
 	pthread_create(&eth_tid[0], NULL, eth_handler, connections);
 	pthread_create(&listen_tid, NULL, listen_handler, &listenfd);
+	/**
+	  *	The main thread will be dedicated to prompting the user for the next
+	  *	proxy to connect to.
+	  */
+	for(i=next_conn; next_conn<CONNECTION_MAX;){
+		unsigned int j, k=0;
+		do{
+			printf("enter address and port of proxy to connect to.\n");
+			fgets(buf, 64, stdin);
+			j=strtoul(buf, bufp, 10);
+			strtok(buf, " \n");
+			bufp=strtok(NULL, " \n");
+			bufp=strtok(NULL, " \n");
+			if(j==ULONG_MAX&&errno==ERANGE
+				||j<1024||j>65535){
+				fprintf(stderr, "error: invalid port parameter: %s\n",
+					strerror(errno));
+				k=0;
+				continue;
+			}
+			port=(unsigned short)j;
+			if((connections[i]=open_clientfd(buf, port))<0){
+				fprintf(stderr, "error opening ethernet device\n");
+				k=0;
+				continue;
+			}
+			k=1;
+		}while(!k);
+		/**
+	  	  *	If this thread created a connection with a higher index than
+		  *	max_conn.
+		  */
+		if(i>max_conn)
+			max_conn=i;
+		//	If next_conn has not changed due to a client disconnection.
+		if(i==next_conn)
+			++i;
+	}
 	pthread_join(tap_tid, NULL);
 	pthread_join(eth_tid[0], NULL);
 	pthread_join(listen_tid, NULL);
