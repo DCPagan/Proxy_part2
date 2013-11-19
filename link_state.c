317b5111417e13e8fe309d56c18f2d64779c171a
Jonathan Chen <jonchen1@pwd.cs.rutgers.edu>
2013-11-19 00:37:14 -0500

    linked list methods

--- link_state.c
+++ link_state.c
@@ -1,15 +1,30 @@
+q
 #include "proxy.h"
 #include "link_state.h"
 
 List *hash_table = NULL;
 
-void remove_member(char* mac, List node){
+List *ll_create(){
+	List *head = malloc(sizeof(*head));
+	head = NULL;
+	return head;
+}
+
+void ll_add(List *list, List *node){
+	LL_APPEND(list, node);
+}
+
+void ll_remove(List *list, List *node){
+	LL_DELETE(list, node);
+}
+
+void remove_member(char* mac, List *node){
 	List *tmp;
 	HASH_FIND_STR(hash_table, mac, tmp);
 	HASH_DEL(hash_table, tmp);
 }
 
-void add_member(char* mac, List node){
+void add_member(char* mac, List *node){
 	List *tmp;
 	HASH_FIND_STR( hash_table, mac, tmp);
 	if(tmp == NULL){
