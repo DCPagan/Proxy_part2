c7274846bda187aac1ddcf258d52ebd79c476484
Jonathan Chen <jonchen1@pwd.cs.rutgers.edu>
2013-11-18 21:59:48 -0500

    added uthash and new structs

---
+++ link_state.h
@@ -0,0 +1,29 @@
+#include <pthread.h>
+#include "utlist.h"
+#include "uthash.h"
+
+typedef struct Config{
+	int listen_port;
+	int link_period;
+	int link_timeout;
+	char* tap;
+	int quit_timer;
+} Config;
+
+typedef struct List{
+	char* hostname;
+	Config config;
+	int port;
+	List *next;
+} List;
+
+typedef struct Graph{
+	List *list;
+	int size;
+	UT_hash_handle hh;
+	pthread_mutex_t lock;
+} Graph;
+
+void remove_member(Graph graph, List node);
+void remove_expired_member(Graph graph, List node);
+void add_member(Graph graph, List node);
