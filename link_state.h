473784e92f8115c1ecfbbe81459df672f82df113
jonchen1 <jonchen1@gmail.com>
2013-11-20 17:50:55 -0500

    Peer

--- link_state.h
+++ link_state.h
@@ -1,17 +1,22 @@
 #include <pthread.h>
 #include "utlist.h"
 #include "uthash.h"
+#include "proxy.h"
 
 typedef struct Config{
 	char mac[6];
 	int listen_port;
 	int link_period;
 	int link_timeout;
-	char* tap;
+	int tap;
 	int quit_timer;
+} Config;
+
+typedef struct Peer{
+	link_state ls;
 	pthread_mutex_t lock;
 	UT_hash_handle hh;
-} Config;
+} Peer;
 
 /*creates a head node for each list and initialized it to null*/
 /*Not needed for part 2*/
@@ -19,6 +24,6 @@ typedef struct Config{
 //void ll_add(List list, List *node);
 //void ll_remove(List list, List *node);
 
-void remove_member(char* Mac, Config *node);
-//void remove_expired_member(char* mac, List *node);
-void add_member(char* Mac, Config *node);
+void remove_member(char* Mac, Peer *node);
+//void remove_expired_member(char* mac, List *node)nk;
+void add_member(char* Mac, Peer *node);
